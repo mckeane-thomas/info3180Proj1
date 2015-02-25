6450449bd74327a585c11d0928b534797c5a60a3
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 00:03:59 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-from app import views
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://action@localhost/action'
+
+db=SQLAlchemy(app)
+
+from app import views,models
