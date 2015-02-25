7815500cabb7bc6f8509b856465c6a99fdb9f3e4
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-24 23:18:37 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 #from flask.ext.uploads import UploadSet, IMAGES
 from flask.ext.wtf import Form
 from wtforms.fields import TextField, IntegerField, DateField, RadioField
-from wtforms.validators import Required, Optional, FileField, file_required
+from wtforms.validators import Required, Optional, file_required
 from flask_wtf.file import FileField, FileAllowed, FileRequired
  
 class RegisterForm(Form):
