5426a44e33ce20530d6349878305fb254582842e
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 10:35:28 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
- import os
-    _basedir = os.path.abspath(os.path.dirname(__file__))
+import os
+ 
+DEBUG = False
 
-    DEBUG = False
+SECRET_KEY = 'This string will be replaced with a proper key in production.'
 
-    SECRET_KEY = 'This string will be replaced with a proper key in production.'
+SQLALCHEMY_DATABASE_URI='postgresql://action@localhost/action'
+DATABASE_CONNECT_OPTIONS = {}
 
-    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://action@localhost/action'
-    DATABASE_CONNECT_OPTIONS = {}
+THREADS_PER_PAGE = 8
 
-    THREADS_PER_PAGE = 8
-
-    CSRF_ENABLED = True
-    CSRF_SESSION_KEY = "secretkey"
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = "secretkey"
