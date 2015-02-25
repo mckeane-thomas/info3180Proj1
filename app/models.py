c2b3506a5be12197860a716acaec255aa295a1c3
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 00:05:50 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class User(db.Model):
 	__tablename__ = 'users_user'
         userid = db.Column(db.Integer,nullable = False, primary_key=True)
-	userName = db.Column(db.String(50), Unique=True)
+	userName = db.Column(db.String(50), unique=True)
 	img = db.Column(db.LargeBinary)
         fname = db.Column(db.String(50), unique=True)
         lname = db.Column(db.String(50), unique=True)
