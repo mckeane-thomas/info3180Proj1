d4804d97abb2d140d6b61927562bb935c91a3a6a
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-26 17:20:56 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,32 +2,32 @@ from app import db
 
 class Profile(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(50), unique=True)
+	#username = db.Column(db.String(50), unique=True)
 	#img = db.Column(db.LargeBinary)
         fname = db.Column(db.String(50), unique=True)
         lname = db.Column(db.String(50), unique=True)
-        sex=db.Column(db.String(10))
-	age = db.Column(db.Integer)
+        #sex=db.Column(db.String(10))
+	#age = db.Column(db.Integer)
 	#profile_add_on=db.Column(db.DateTime)
-	high_score =db.Column(db.Integer)
-	tDollars =db.Column(db.Integer)
+	#high_score =db.Column(db.Integer)
+	#tDollars =db.Column(db.Integer)
 	
 
 
-	def __init__(self, username,fname, lname,sex,age,high_score,tDollars):
+	def __init__(self,fname, lname):
 	   #self.userid = userid
 	   self.username=username 
 	   #self.img = img
 	   self.fname = fname
            self.lname = lname
            #self.profile_add_on=profile_add_on
-           self.sex = sex
-           self.age = age
-           self.high_score=high_score
-           self.tDollars=tDollars
+           #self.sex = sex
+           #self.age = age
+           #self.high_score=high_score
+           #self.tDollars=tDollars
            
           
            
 
         def __repr__(self):
-	    return '<User %r>' %(self.username)
+	    return '<Profile %r %r>' %(self.fname, self.lname)
