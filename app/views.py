e070e5dabe823abae7fdfe85feadc3a28ad89f25
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 23:30:18 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -61,7 +61,7 @@ def page_not_found(error):
    return render_template('404.html'), 404
     
 
-@app.route('/profile/')
+@app.route('/profile/', method =['POST','GET')
 def profile_add():
    #route for adding a profile
    """adding a profile single Profile."""
