06aed6b90a0893cf3789433695866092ccd711d1
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 23:55:38 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -69,7 +69,7 @@ def profile_add():
    if request.method =="POST":
       #write to the database
       #fname = request.form
-      if form.validate_on_submit():
+      #if form.validate_on_submit():
       user = User(userid = form.userid.data, username=form.username.data, img=form.img.data,fname=form.fname.data,lname=form.lname.data, sex=form.sex.data, age=form.age.data, profile_add_on=form.profile_add_on.data)
       #db.session.add(user)
       #db.session.commit()
