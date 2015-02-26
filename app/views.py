6dba8595ec3c2232b5278bdcacb29064368476f3
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 23:43:49 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -65,17 +65,18 @@ def page_not_found(error):
 def profile_add():
    #route for adding a profile
    """adding a profile single Profile."""
-   form = RegisterForm(request.form)
-   if form.validate_on_submit():
+   form = RegisterForm()
+   if request.method =="POST":
+      #if form.validate_on_submit():
       #user = User(userid = form.userid.data, username=form.username.data, img=form.img.data,fname=form.fname.data,lname=form.lname.data,sex=form.sex.data,age=form.age.data,profile_add_on=form.profile_add_on.data)
       #db.session.add(user)
       #db.session.commit()
 
       #session['user_id']=user.id
       return "Post Successful"   
-      flash('You have been registered')
+      #flash('You have been registered')
     
-      return redirect(url_for('home'))
+      #return redirect(url_for('home'))
    return render_template("profile_add.html", form=form) 
   
 @app.route('/profiles/')
