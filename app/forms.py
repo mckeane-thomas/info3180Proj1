bb204710e27f745fce9f522748f83f2e3da44c59
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-25 22:50:24 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,7 @@ class RegisterForm(Form):
       #profile_photo =FileField('Profile Image')
 	userid =IntegerField('ID:', [Required()])
 	username = TextField('Username:', [Required()])
-	upload = FileField('image', validators=[FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
+	img = FileField('image', validators=[FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
 	fame = TextField('First Name:', [Required()])
 	lname = TextField('Last Name:', [Required()])
 	sex = RadioField(u'Sex', choices=[
