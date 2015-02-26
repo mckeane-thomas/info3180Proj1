4d9fdf282414cfce80fbd3919a373cbe3a697147
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-26 17:27:59 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -78,7 +78,7 @@ def profile_add():
       high_score=request.form['high_score']
       tDollars = request.form['tDollars']
       #profile_add_on = request.form['profile_add_on']
-      newprofile = Profile(username,fname,lname,sex,age,high_score,tDollars)
+      newprofile = Profile(fname,lname)
       #user = User(username=form.username.data, fname=form.fname.data,lname=form.lname.data, sex=form.sex.data, age=form.age.data, high_score=form.high_score.data, tDollars=form.tDollars.data)
       db.session.add(newprofile)
       db.session.commit()
