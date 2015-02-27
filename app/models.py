2286225f27b1bdc58d605f02510e64626a845e8e
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 16:12:03 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 
-class User(db.Model):
+class Profile(db.Model):
         id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True)
 	#img = db.Column(db.LargeBinary)
