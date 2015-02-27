c298ffedf7262cbcfabde4f3f67ee02f60d4c32f
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 10:59:28 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,29 +2,29 @@ from app import db
 
 class Profile(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-	#username = db.Column(db.String(50), unique=True)
-	#img = db.Column(db.LargeBinary)
+	username = db.Column(db.String(50), unique=True)
+	img = db.Column(db.LargeBinary)
         fname = db.Column(db.String(50), unique=True)
         lname = db.Column(db.String(50), unique=True)
-        #sex=db.Column(db.String(10))
-	#age = db.Column(db.Integer)
-	#profile_add_on=db.Column(db.DateTime)
-	#high_score =db.Column(db.Integer)
-	#tDollars =db.Column(db.Integer)
+        sex=db.Column(db.String(10))
+	age = db.Column(db.Integer)
+	profile_add_on=db.Column(db.DateTime)
+	high_score =db.Column(db.Integer)
+	tDollars =db.Column(db.Integer)
 	
 
 
-	def __init__(self,fname, lname):
+	def __init__(self,,username,img,fname, lname,profile_add_on,sex,age,high_score,tDollars):
 	   #self.userid = userid
-	   #self.username=username 
-	   #self.img = img
+	   self.username=username 
+	   self.img = img
 	   self.fname = fname
            self.lname = lname
-           #self.profile_add_on=profile_add_on
-           #self.sex = sex
-           #self.age = age
-           #self.high_score=high_score
-           #self.tDollars=tDollars
+           self.profile_add_on=profile_add_on
+           self.sex = sex
+           self.age = age
+           self.high_score=high_score
+           self.tDollars=tDollars
            
           
            
