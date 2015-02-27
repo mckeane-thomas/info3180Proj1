faf51f1034b844fa8e8fe2f304592da10490febe
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 15:07:32 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ from flask import render_template,request,redirect,url_for,flash, session
 from app.forms import RegisterForm
 from app import db
 from app.models import Profile
+import time
 
 
 ###
