893326f990774b400bfa9967d144a794afdffa15
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 13:14:02 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -18,6 +18,6 @@ class RegisterForm(Form):
         ('F', u'Female')],
         default='M', validators=[Required()])
 	age = IntegerField('Age:', [Required()])
-	profile_add_on = DateField('Profile Created',[Required()])
+	#profile_add_on = DateField('Profile Created',[Required()])
 	high_score = IntegerField('High Score', [Optional()])
 	tDollars = IntegerField('TDollars', [Optional()])
