9769468debaf2f43788b6b95e2c60e1af6ae9286
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 13:13:10 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -93,7 +93,7 @@ def profile_add():
       return render_template("profile_add.html", form=form) 
 
 def dateAdded():
-   date_add=time.strftime("%d,%m,%y,%t")
+   date_add=time.strftime("%a,%d,%b,%Y")
    return date_added
   
 @app.route('/profiles/')
