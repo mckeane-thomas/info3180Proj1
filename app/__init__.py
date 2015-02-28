d39f8bd5e33c10cb770ce97369b33313ae6b5c07
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-27 23:14:05 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://action@localhost/action'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://dnxdmynmggyqte:HvuEMaKeFu0t6nvxcmNuf0wUIV@ec2-23-23-180-133.compute-1.amazonaws.com:5432/d255jkf0u2kot'
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY']="sdfhdjksfhgm nm,sfnjkdfsbdjbfkjsbbjasbhdsbfd"
