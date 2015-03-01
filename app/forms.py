341208985f6370357610f78428105b5c0f0f86a5
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-28 22:39:47 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask.ext.wtf import Form
+from flask.ext.uploads import UploadSet, IMAGES
 from wtforms.fields import TextField, IntegerField, DateField, RadioField
 from wtforms.validators import Required, Optional
 from flask_wtf.file import FileField, FileAllowed, FileRequired
  
-#images = UploadSet('images', IMAGES)
+images = UploadSet('images', IMAGES)
 
 class RegisterForm(Form):
       #profile_photo =FileField('Profile Image')
 	userid =IntegerField('ID:', [Required()])
 	username = TextField('Username:', [Required()])
-	img = FileField('image', validators=[Optional(),FileAllowed(['jpg', 'png'], 'Images only!')])
+	img = FileField('image', validators=[Optional(),FileAllowed(images, 'Images only!')])
 	fname = TextField('First Name:', [Required()])
 	lname = TextField('Last Name:', [Required()])
 	sex = RadioField(u'Sex', choices=[
