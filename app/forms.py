4ae91b6da223d42542937997fef83f62c8e856d5
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-28 22:42:44 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from flask.ext.wtf import Form
-from flask.ext.uploads import UploadSet, IMAGES
+#from flask.ext.uploads import UploadSet, IMAGES
 from wtforms.fields import TextField, IntegerField, DateField, RadioField
 from wtforms.validators import Required, Optional
 from flask_wtf.file import FileField, FileAllowed, FileRequired
  
-images = UploadSet('images', IMAGES)
+#images = UploadSet('images', IMAGES)
 
 class RegisterForm(Form):
       #profile_photo =FileField('Profile Image')
 	userid =IntegerField('ID:', [Required()])
 	username = TextField('Username:', [Required()])
-	img = FileField('image', validators=[Optional(),FileAllowed(images, 'Images only!')])
+	img = FileField('image', validators=[Optional(),FileAllowed(['jpg', 'png'], 'Images only!')])
 	fname = TextField('First Name:', [Required()])
 	lname = TextField('Last Name:', [Required()])
 	sex = RadioField(u'Sex', choices=[
@@ -19,6 +19,6 @@ class RegisterForm(Form):
         ('F', u'Female')],
         default='M', validators=[Required()])
 	age = IntegerField('Age:', [Required()])
-	#profile_add_on = DateField('Profile Created',[Required()])
+	profile_add_on = DateField('Profile Created',[Required()])
 	high_score = IntegerField('High Score', [Optional()])
 	tDollars = IntegerField('TDollars', [Optional()])
