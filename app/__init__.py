1a14522189b58a30ce72705575dc499c0d2a55dc
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-01 21:21:47 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'app/static/img/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #app.config['SQLALCHEMY_DATABASE_URI']='postgres://dnxdmynmggyqte:HvuEMaKeFu0t6nvxcmNuf0wUIV@ec2-23-23-180-133.compute-1.amazonaws.com:5432/d255jkf0u2kot'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://action@localhost/action'
 db = SQLAlchemy(app)
