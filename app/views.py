638e7aaa35bf3e5ac7c958cfe352a69c80161e26
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-02 14:18:59 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -67,7 +67,7 @@ def profile_add():
    #route for adding a profile
    """adding a profile single Profile."""
    form = RegisterForm()
-   if request.method == 'POST' and form.validate_on_submit():
+   if request.method == 'POST':
       #write to the database
       name = request.form['username']
       fname = request.form['fname']
