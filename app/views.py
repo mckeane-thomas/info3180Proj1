c7641d10fb17c4449d4d7ec9084d8ba40c5b7551
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-01 23:19:39 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -86,8 +86,8 @@ def profile_add():
       #session['user_id']=user.id
       return "Registration Completed values added to the database"  
       flash("New Profile added")
-   else:
-      return("Profile could not be Created Fill in required fields")
+   #else:
+    #  return("Profile could not be Created Fill in required fields")
    #else:  
    #return redirect(url_for('home'))
    return render_template("profile_add.html", form=form) 
