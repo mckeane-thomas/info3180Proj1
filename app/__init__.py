c952b3e90ab8ff6107db3b469ae23e0f36b38461
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-02 16:48:32 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-UPLOAD_FOLDER = 'app/static/img/profile_pics'
+UPLOAD_FOLDER = 'app/static/img/'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
